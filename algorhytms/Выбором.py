37f90fffb5e4feaa61eb86d1075ef723d22b793c
brestyck <47777597+brestyck@users.noreply.github.com>
2023-01-19 21:59:17 +0300

    Delete Выбором.py

--- algorhytms/Выбором.py
+++
@@ -1,9 +0,0 @@
-def selection_sort(a):
-    for i in range(0, len(a)-1):
-        for j in range(i+1, len(a)):
-            if a[j] < a[i]:
-                a[j], a[i] = a[i], a[j]
-    return a
-
-array = [51, 9, 0, 2, -1, 43]
-print(selection_sort(array))
